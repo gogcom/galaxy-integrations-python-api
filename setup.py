48e54a8460c15fcee3368abcec080ec88797f76e
Paweł Kierski <pkierski@gog.com>
2019-02-20 14:09:34 +0100

    Revert "Make galaxy namespace package"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="galaxy.plugin.api",
-    version="0.4",
+    version="0.5",
     description="Galaxy python plugin API",
     author='Galaxy team',
     author_email='galaxy@gog.com',
