4adef2dace58d340107383b38ccbff875f521b80
Romuald Juchnowicz-Bierbasz <rbierbasz@gog.com>
2019-04-16 10:38:45 +0200

    SDK-2760: Move modules

--- tests/async_mock.py
+++
@@ -1,6 +0,0 @@
-from unittest.mock import MagicMock
-
-class AsyncMock(MagicMock):
-    async def __call__(self, *args, **kwargs):
-        # pylint: disable=useless-super-delegation
-        return super(AsyncMock, self).__call__(*args, **kwargs)
