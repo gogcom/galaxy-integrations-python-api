3b296cbcc912a6b7192dba5fc30c1f17249f8abc
Romuald Juchnowicz-Bierbasz <rbierbasz@gog.com>
2019-05-21 14:18:45 +0200

    Fix pipeline

--- jenkins/release.groovy
+++ jenkins/release.groovy
@@ -1,13 +1,13 @@
 stage('Upload to github')
 {
-    node('ActiveClientWindowsBuilder') {
+    node('ActiveClientMacosxBuilder') {
         deleteDir()
         checkout scm
         withPythonEnv('python') {
             withCredentials([usernamePassword(credentialsId: 'github_friendsofgalaxy', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
-                bat 'pip install -r jenkins/requirements.txt'
-                def version = bat(returnStdout: true, script: 'python setup.py --version').trim()
-                bat "python jenkins/release.py $version"
+                sh 'pip install -r jenkins/requirements.txt'
+                def version = sh(returnStdout: true, script: 'python setup.py --version').trim()
+                sh "python jenkins/release.py $version"
             }
         }
     }
