f97b6c8971fdaef7dfc1139848c18bc12a98cfaa
GOG Galaxy SDK Team <galaxy-sdk@gog.com>
2019-05-31 12:09:18 +0200

    version 0.31.3

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="galaxy.plugin.api",
+    version="0.31.3",
+    description="Galaxy python plugin API",
+    author='Galaxy team',
+    author_email='galaxy@gog.com',
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    install_requires=[
+        "aiohttp==3.5.4",
+        "certifi==2019.3.9"
+    ]
+)
