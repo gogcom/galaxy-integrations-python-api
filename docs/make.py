192d655d5133c1c03da087735c26fe03e690d8a3
GOG Galaxy SDK Team <galaxy-sdk@gog.com>
2019-06-10 19:04:08 +0200

    version 0.32.1

--- docs/make.py
+++ docs/make.py
@@ -1,13 +1,16 @@
 """Builds documentation locally. Use for preview only"""
 
+import pathlib
 import subprocess
 import webbrowser
 
 
-source = "docs/source"
-build = "docs/build"
-master_doc = 'index'
+source = pathlib.Path("docs", "source")
+build = pathlib.Path("docs", "build")
+master_doc = 'index.html'
 
-subprocess.run(['sphinx-build', '-M', 'clean', source, build])
-subprocess.run(['sphinx-build', '-M', 'html', source, build])
-webbrowser.open(f'{build}/html/{master_doc}')
+subprocess.run(['sphinx-build', '-M', 'clean', str(source), str(build)])
+subprocess.run(['sphinx-build', '-M', 'html', str(source), str(build)])
+
+master_path = build / 'html' / master_doc
+webbrowser.open(f'file://{master_path.resolve()}')
