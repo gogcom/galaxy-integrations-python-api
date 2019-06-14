7789927ed9b409f7211e3fb18cae90cdffefe46f
GOG Galaxy SDK Team <galaxy-sdk@gog.com>
2019-06-14 16:54:11 +0200

    version 0.34

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="galaxy.plugin.api",
-    version="0.33.1",
+    version="0.34",
     description="GOG Galaxy Integrations Python API",
     author='Galaxy team',
     author_email='galaxy@gog.com',
