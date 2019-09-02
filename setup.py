bab0be9994ecb36c77ada89a7f9135c6db0324d7
Romuald Juchnowicz-Bierbasz <rbierbasz@gog.com>
2019-09-02 17:31:47 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="galaxy.plugin.api",
-    version="0.47",
+    version="0.48",
     description="GOG Galaxy Integrations Python API",
     author='Galaxy team',
     author_email='galaxy@gog.com',
