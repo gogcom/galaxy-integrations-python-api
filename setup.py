26102dd832a5d13f249393c484466ec135914b52
Aleksej Pawlowskij <apawlowskij@gog.com>
2019-12-17 15:55:32 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="galaxy.plugin.api",
-    version="0.62",
+    version="0.63",
     description="GOG Galaxy Integrations Python API",
     author='Galaxy team',
     author_email='galaxy@gog.com',
